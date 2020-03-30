9b43dfcafccc9a1991c66fd8645332583f5a6842
henriqueLimaSanto <henriquegualberto33@gmail.com>
2020-03-29 23:23:11 -0300

    teste

--- RequestWebCSharp/RequestWebCSharp/Program.cs
+++ RequestWebCSharp/RequestWebCSharp/Program.cs
@@ -9,7 +9,7 @@ namespace RequestWebCSharp
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main Hnerique hteste entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
